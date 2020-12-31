23b6098a2d347f1c723aeb18274791b19e982747
YunheeWoo <unhwoo@naver.com>
2021-01-01 03:03:34 +0900

    fix

--- etc/npy2img.py
+++ etc/npy2img.py
@@ -2,53 +2,86 @@ import os
 import argparse
 import numpy as np
 from pathlib import Path
+import datetime
 
 width = 2000
 height = 100
 
-def drawimg(data, path, w, h):
+def draw_img(data, path, ann, width, height, norm):
 
+    # preprocessing
+    if norm == "min-max-cut" and norm == "mean-std-cut":
         cut_value = 192*1e-06
+        data = np.where(data < -cut_value, -cut_value, data)
+        data = np.where(data > cut_value, cut_value, data)
 
-        for file_idx, file in enumerate(signal_files):
-            data = np.load(npy_path+folder+'/'+file)[1]
-            #data = (data - np.min(data)) / (np.max(data) - np.min(data))
-            data = np.where(data < -cut_value, -cut_value, data)
-            data = np.where(data > cut_value, cut_value, data)
-            data = (data - np.min(data)) / (np.max(data) - np.min(data))
-            data = np.reshape(data,(-1,6000))
-            annotation = np.load(annotation_path+file)
-            
-            for j in range(data.shape[0]):
-                plt.figure(figsize=(img_width/100,img_height/100), dpi=100)
-                plt.ylim(0, 1)
-                plt.xlim(0,6000)
-                plt.box(on=None)
-                plt.axis('off')
-                plt.tight_layout()
-                plt.subplots_adjust(left = 0, bottom = 0, right = 1, top = 1, hspace = 0, wspace = 0)
-                plt.plot(data[j], linewidth=1)
-                plt.savefig(img_path+folder+'/'+str(annotation[j])+'/'+str(img_num).zfill(6)+".png")
-                plt.close('all')
-                plt.cla()
-                plt.clf()
-                img_num += 1
+    if norm == "min-max-discard" and norm == "mean-std-discard":
+        m = 3.5
+        std = np.std(data)
+        idx1 = (data - np.mean(data)) > m * np.std(data)
+        idx2 = (data - np.mean(data)) < -m * np.std(data)
+        data[idx1] = m * std
+        data[idx2] = -m * std
 
-            print(file + " done")
+    data = np.reshape(data,(-1,6000))
+    annotation = np.load(annotation_path+file)
+
+    if not data.shape[0] == ann[0]:
+        print("data shape and annotation do not match")
+        return
+
+    img_num = 0
+
+    for d_idx in range(data.shape[0]):
+        plt.figure(figsize=(width/300,height/300), dpi=300)
+        plt.ylim(np.min(data), np.max(data))
+        plt.xlim(0,6000)
+        plt.box(on=None)
+        plt.axis('off')
+        plt.tight_layout()
+        plt.subplots_adjust(left = 0, bottom = 0, right = 1, top = 1, hspace = 0, wspace = 0)
+        plt.plot(data[d_idx], linewidth=1, color="black")
+        img_name = str(img_num).zfill(4) + "_" + ann[d_idx] + ".png"
+        plt.savefig(path / img_name)
+        plt.close('all')
+        plt.cla()
+        plt.clf()
+        img_num += 1
 
 src_path = Path("/home/eslab/wyh/data/npy")
 dst_path = Path("/home/eslab/wyh/data/img")
 ann_path = Path("/home/eslab/wyh/data/annotations")
 
+img_size = width + "x" + height
+
+normalization = ["min-max-cut", "mean-std-cut", "min-max-discard", "min-max-discard", "original", "min-max", "mean-std"]
+
 signal_list = ["EMG", "C3-M2", "C4-M1", "E1-M2", "E2-M1", "F3-M2", "F4-M1", "O1-M2", "O2-M1"]
 
 patients = os.listdir(src_path)
 patients.sort()
-for p in patients:
-    os.makedirs(dst_path+p, exist_ok=True)
-    datas = os.listdir(src_path / p)
-    datas.sort()
-    for data in datas:
-        epoch = np.load(src_path / p / data)
+
+for norm in normalization:
+    for p in patients:
+        print("===========" + p + " start ===========")
+
+        datas = np.load(src_path / p)
+        ann = np.load(ann_path / p)
+
+        for sig_idx, signal in enumerate(signal_list):
+            if signal == "F3-M2":
+                print("> F3-M2 skip")
+                continue
+            if signal == "F4-M1":
+                print("> F4-M1 skip")
+                continue
+            
+            print("- " + signal + " start")
+
+            os.makedirs(dst_path / img_size / signal / p.split(".")[0], exist_ok=True)
+            
+            draw_img(data[sig_idx], dst_path / img_size / signal / p.split(".")[0], width, height)
+
+            print("- " + signal + " end")
 
 
